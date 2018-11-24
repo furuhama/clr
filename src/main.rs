4c067160110f4d633d3651444b81c1341b10fe49
furuhama <furuhama@users.noreply.github.com>
2018-11-24 18:11:40 +0900

    main() returns Result<(), Box<Error>> & Use `?` operator to unwrap value

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate csv;
 use std::fs::File;
 use std::io::BufReader;
 use std::env;
+use std::error::Error;
 
 const ANSI_COLOR_CODES: [&str; 8] = [
     "\x1b[31m", // Red
@@ -15,18 +16,20 @@ const ANSI_COLOR_CODES: [&str; 8] = [
     "\x1b[0m",  // Reset all attributes
 ];
 
-fn main() {
+fn main() -> Result<(), Box<Error>> {
     let filename = env::args().nth(1).unwrap();
-    let file = File::open(filename).unwrap();
+    let file = File::open(filename)?;
     let buf_reader = BufReader::new(file);
     let mut rdr = csv::Reader::from_reader(buf_reader);
 
-    for result in rdr.records() {
-        let record = result.unwrap();
+    for record in rdr.records() {
+        let record = record?;
         println!("{:?}", record);
     }
 
     for color in ANSI_COLOR_CODES.iter() {
         println!("{}test text", color);
     }
+
+    Ok(())
 }

fe32704bccee784b503032d17c4b429cd79b5778
Yusuke Furuhama <furuhama@users.noreply.github.com>
2026-06-02 17:23:26 +0900

    Display line-number with --line-number/-n option

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,15 @@ const ANSI_COLOR_CODES: [&str; 7] = [
 ];
 
 const RESET_CODE: &str = "\x1b[0m";
+const LINE_NUM_COLOR: &str = "\x1b[2m"; // Dim
 
-fn colorize_row(row: &StringRecord) {
+fn colorize_row(row: &StringRecord, line_num: Option<usize>, width: usize) {
+    if let Some(n) = line_num {
+        print!("{}{:>width$} | {}", LINE_NUM_COLOR, n, RESET_CODE, width = width);
+    } else {
+        print!("{}{:>width$} | {}", LINE_NUM_COLOR, "#", RESET_CODE, width = width);
+    }
+
     for (idx, elem) in row.iter().enumerate() {
         let color_code = ANSI_COLOR_CODES[idx % ANSI_COLOR_CODES.len()];
         print!("{}{} ", color_code, elem);
@@ -29,39 +36,70 @@ fn colorize_row(row: &StringRecord) {
     println!("{}", RESET_CODE);
 }
 
+fn colorize_row_plain(row: &StringRecord) {
+    for (idx, elem) in row.iter().enumerate() {
+        let color_code = ANSI_COLOR_CODES[idx % ANSI_COLOR_CODES.len()];
+        print!("{}{} ", color_code, elem);
+    }
+
+    println!("{}", RESET_CODE);
+}
+
+struct Args {
+    filename: Option<String>,
+    show_line_numbers: bool,
+}
+
+fn parse_args() -> Args {
+    let mut filename = None;
+    let mut show_line_numbers = false;
+
+    for arg in env::args().skip(1) {
+        match arg.as_str() {
+            "-n" | "--line-number" => show_line_numbers = true,
+            s if s.starts_with('-') => {}
+            _ => filename = Some(arg),
+        }
+    }
+
+    Args { filename, show_line_numbers }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // Read input data, and put them all on buffer at once.
-    // (If an input data size gets bigger,
-    // this code may cause performance problem.
-    // It should be refactored to processing data as a stream.)
+    let args = parse_args();
     let mut buf = Vec::new();
 
-    // If an argument is given,
-    // it will be taken as a filename of target CSV file.
-    // Or, it will be taken as an input data ig given from STDIN.
-    if let Some(filename) = env::args().nth(1) {
+    if let Some(filename) = args.filename {
         let file = File::open(filename)?;
         let mut buf_reader = BufReader::new(file);
-
         buf_reader.read_to_end(&mut buf)?;
     } else {
         let mut input = io::stdin();
-
         input.read_to_end(&mut buf)?;
     };
 
-    // Even if input `buf` variable data encoding is `UTF-8` already,
-    // it does not raise an error on the decoding process written below.
     let (buf_as_utf8, _, _) = SHIFT_JIS.decode(&buf);
 
     let mut rdr = csv::Reader::from_reader(buf_as_utf8.as_bytes());
 
-    let header = rdr.headers()?;
-    colorize_row(&header);
+    if args.show_line_numbers {
+        let records: Vec<StringRecord> = rdr.records().collect::<Result<_, _>>()?;
+        let width = records.len().to_string().len().max(1);
 
-    for record in rdr.records() {
-        let record = record?;
-        colorize_row(&record);
+        let header = rdr.headers()?;
+        colorize_row(&header, None, width);
+
+        for (i, record) in records.iter().enumerate() {
+            colorize_row(record, Some(i + 1), width);
+        }
+    } else {
+        let header = rdr.headers()?;
+        colorize_row_plain(&header);
+
+        for record in rdr.records() {
+            let record = record?;
+            colorize_row_plain(&record);
+        }
     }
 
     Ok(())
